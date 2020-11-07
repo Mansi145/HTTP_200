51b92c50bc60779f5827b547a4075c4f8de443c0
Mansi Gupta <guptamansi9350@gmail.com>
2020-11-07 21:17:33 +0530

    added dummy course data

--- apps/profiles/management/commands/createdata.py
+++ apps/profiles/management/commands/createdata.py
@@ -2,6 +2,11 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 from faker import Faker
 from django.contrib.auth.models import User
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname("createdata.py"), '../../..'))
+from profiles.models import StudentDetail, FacultyDetail
+from questionPapers.models import Course, Branch, Year, Subject
 
 
 class Command(BaseCommand):
@@ -10,6 +15,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
         parser.add_argument('--dummydata', action='store_true', help='Create dummy data')
+        parser.add_argument('--coursedata', action='store_true', help='Create dummy course data')
 
     def handle(self, *attrs, **options):
 
@@ -42,3 +48,55 @@ class Command(BaseCommand):
                     users_group = Group.objects.get(name=group_name)
                     users_group.user_set.add(user)
                     user.save()
+                    user.save()
+                    if (str(group_name) == "student"):
+                        StudentDetail.objects.create(user=user)
+                    else:
+                        FacultyDetail.objects.create(user=user)
+
+        if options['coursedata']:
+
+            # Create Course Instances
+            courses = {'BTech': 'Bachelor of Technology',
+                       'MTech': 'Master of Engineering',
+                       'MBA': 'Master of Business Administration'}
+
+            for courseCode, courseName in courses.items():
+                Course.objects.create(courseCode=courseCode, courseFullName=courseName)
+
+            # Creates Branch Instances
+            branchesBTech = {'CSE': 'Computer Science and Engineering',
+                             'ECE': 'Electrical & Computer Engineering'}
+
+            branchesMTech = {'PE': 'Production Engineering'}
+
+            branchesMBA = {'KMBHR': 'Human Resource',
+                           'KMBIB': 'International Business'}
+
+            for branchCode, branchName in branchesBTech.items():
+                Branch.objects.create(course=Course.objects.get(courseCode='BTech'), deptCode=branchCode, deptFullName=branchName)
+
+            for branchCode, branchName in branchesMTech.items():
+                Branch.objects.create(course=Course.objects.get(courseCode='MTech'), deptCode=branchCode, deptFullName=branchName)
+
+            for branchCode, branchName in branchesMBA.items():
+                Branch.objects.create(course=Course.objects.get(courseCode='MBA'), deptCode=branchCode, deptFullName=branchName)
+
+            # Create Years
+            for year in range(1, 5):
+                Year.objects.create(number=year)
+
+            # Create Subjects
+            subjects = [['CSE', 'KCS403', 'Microprocessor', 2],
+                        ['CSE', 'KCS601', 'Software Engineering', 3],
+                        ['ECE', 'KEC501', 'Integrated Circuits', 3],
+                        ['ECE', 'REC702', 'VLSI Design', 4],
+                        ['PE', 'MTME101', 'Simulation, Modelling & Analysis', 1],
+                        ['KMBHR', 'KMB301', 'Strategic Management', 1],
+                        ['KMBIB', 'KMBIB04', 'International Trade Laws', 2]]
+
+            for subject in subjects:
+                Subject.objects.create(deptName=Branch.objects.get(deptCode=subject[0]),
+                                       yearNum=Year.objects.get(number=subject[3]),
+                                       subCode=subject[1],
+                                       subName=subject[2])
